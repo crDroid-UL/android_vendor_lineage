cd32c622012bed2e9d90f7c64d8fd1fdd3691709
Khalvat-M <mmba313@gmail.com>
2022-10-25 06:45:07 +0330

    Revert "config: Remove TARGET_HAS_MEMFD_BACKPORT"
    This reverts commit e020bffce2a1f5a0ba074cede2ce072221a5a6eb.

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,6 +37,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     camera_needs_client_info_lib \
     camera_needs_client_info_lib_oplus \
     needs_camera_boottime \
+    has_memfd_backport \
     needs_netd_direct_connect_rule \
     uses_oplus_camera \
     uses_nothing_camera \
@@ -80,6 +81,7 @@ SOONG_CONFIG_lineageGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_PO
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_lineageGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_lineageGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_lineageGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_lineageGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_lineageGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
